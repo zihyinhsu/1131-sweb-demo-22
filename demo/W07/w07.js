7d16124ccb867d2bc821749f2dd47bdb0e6697bc
Zihyin <jenny010328@gmail.com>
2024-10-24 23:29:03 +0800

    Create navbar with links to class demo w02, w03, w06, and deploy it to Vercel

--- demo/W07/w07.js
+++ demo/W07/w07.js
@@ -1,1 +1,14 @@
-alert("hello");
+const p = document.querySelector(".show-classDemo");
+function showDemo(item){
+    switch(item){
+        case "w02_intro":
+            p.innerHTML = `<iframe src="../w02-intro-22/index.html" width="100%" height="100%"></iframe>`
+            break;
+        case "w03_card":
+            p.innerHTML = `<iframe src="../w03_card/index.html" width="100%" height="100%"></iframe>`
+            break;
+        case "w06_blog":
+            p.innerHTML = `<iframe src="../w06_blog/index.html" width="100%" height="100%"></iframe>`
+            break;
+    }
+}
