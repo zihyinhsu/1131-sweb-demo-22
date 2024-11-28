a437fbff20a10878b870e6b0226befb178e9a90c
Zihyin <jenny010328@gmail.com>
2024-11-28 19:39:36 +0800

    W12-P1: Show annser of p1_xx, p2_xx, p3_xx in midterm exam

---
+++ demo/w12_navbar/js/p4.js
@@ -0,0 +1,19 @@
+const p = document.querySelector('.show-midterm-answer');
+
+p.addEventListener('click', function() {
+    
+});
+
+function showMidtermPage(item){
+    switch(item){
+        case 'p1_22':
+            p.innerHTML = `<iframe src="./p1_22/p1_22.html" width="100%" height="100%" />`;
+            break;
+        case 'p2_22':
+            p.innerHTML = `<iframe src="./p2_22/p2_22.html" width="100%" height="100%" />`;
+            break;
+        case 'p3_22':
+            p.innerHTML = `<iframe src="./p3_22/p3_22.html" width="100%" height="100%" />`;
+            break;
+    }
+}
